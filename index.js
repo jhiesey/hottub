5761d55fcf8f1f6eaa54f1c0494d052085aa5d65
John Hiesey <john@hiesey.com>
2016-11-02 16:39:57 -0700

    basic circulation control

--- index.js
+++ index.js
@@ -2,8 +2,54 @@ var sensors = require('./sensors')
 
 sensors.enable(true)
 
+const SENSOR_DELAY = 60 // seconds
+
+// requests:
+// *chemical injection
+// *measurements
+
+// check if enough time has elapsed
 sensors.on('reading', function (reading) {
-	console.log('TEMP:', reading.temp)
-	console.log('PH:', reading.ph)
-	console.log('ORP:', reading.orp)
+	const circDuration = Date.now() - circulationStartTime
+	if (circDuration / 1000 > SENSOR_DELAY) {	
+		console.log('TEMP:', reading.temp)
+		console.log('PH:', reading.ph)
+		console.log('ORP:', reading.orp)
+	}
 })
+
+var circulationRef = 0
+var circulationStartTime
+function enableCirculation () {
+	if (circulationRef++ === 0) {
+		// enable circulation pump here
+		circulationStartTime = Date.now()
+		sensors.enable(true)
+	}
+
+
+	// automatically use the output
+}
+
+function disableCirculation () {
+	if (--circulationRef === 0) {
+		sensors.enable(false)
+	}
+}
+
+/*
+when readings requested, start pump
+after delay, start readings (or start immediately and ignore initially)
+
+when page loaded, request readings
+when websocket dies, stop readings
+
+readings requests should be reference counted
+
+every hour:
+	request readings
+	compute required chemicals
+	inject chlorine (if needed)
+	inject acid (if needed)
+	inject base (if needed)
+*/
